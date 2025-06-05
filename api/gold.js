7c41a2ea43823074309c8a5b65fbd22b2ff9bd5c
Haso0on12 <haso0on12@hotmail.com>
2025-06-05 07:01:29 +0300

    Add files via upload

--- api/gold.js
+++ api/gold.js
@@ -1,10 +1,6 @@
-// /api/gold.js
 export default async function handler(req, res) {
-  const apiKey = '01b928f933da5df8aea4fa0eee5054cb';
-  const url = `https://api.metalpriceapi.com/v1/latest?api_key=${apiKey}&base=USD&currencies=XAU,SAR`;
-
   try {
-    const response = await fetch(url);
+    const response = await fetch("https://api.metalpriceapi.com/v1/latest?api_key=01b928f933da5df8aea4fa0eee5054cb&base=USD&currencies=XAU,SAR");
     const data = await response.json();
 
     const pricePerOunceUSD = 1 / data.rates.XAU;
@@ -14,13 +10,12 @@ export default async function handler(req, res) {
 
     res.status(200).json({
       oz: pricePerOunceUSD.toFixed(2),
-      "24": pricePerGram24K.toFixed(2),
+      "24": (pricePerGram24K).toFixed(2),
       "22": (pricePerGram24K * 0.916).toFixed(2),
       "21": (pricePerGram24K * 0.875).toFixed(2),
       "18": (pricePerGram24K * 0.750).toFixed(2)
     });
   } catch (error) {
-    res.status(500).json({ error: 'فشل في جلب الأسعار' });
+    res.status(500).json({ error: "Failed to fetch gold prices" });
   }
 }
-
